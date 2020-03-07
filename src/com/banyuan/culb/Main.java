8a992afbc243f8e7d38e7e17c69a49131228b4dc
Your Name <you@example.com>
2020-03-07 20:53:59 +0800

    ce

--- src/com/banyuan/culb/Main.java
+++ src/com/banyuan/culb/Main.java
@@ -2,6 +2,6 @@ package com.banyuan.culb;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello word!");
+        System.out.println("hello word。");
     }
 }
