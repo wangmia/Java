8dd51f721f86d463466d38056205c66eb9cf6fce
Your Name <you@example.com>
2020-03-08 18:26:01 +0800

    2-基础练习

---
+++ ceshi.java
@@ -0,0 +1,8 @@
+class Ceshi{
+	public static void main(String[] args){
+	byte a=1,b=2;
+	byte c = a-b;
+	System.out.println(c);
+	}
+}
+
--- src/com/banyuan/culb/P2.java
+++ src/com/banyuan/culb/P2.java
@@ -2,14 +2,33 @@ package com.banyuan.culb;
 
 public class P2 {
     public static void main(String[] args) {
+        // a+b+c+"" 数据类型测试
         int a =3;
         char b = 5;
         double c = 1.0;
         System.out.println(getType(a+b+c+""));
-//        char a = 'a';
+
+        // 字符串转int型测试
         String s = "s$s";
 //        int ss = (int)s;
-        
+
+        double e=9.21D;
+        System.out.println(e);
+
+        // 取余
+        System.out.println(100%3);
+        System.out.println(100%3.0);
+
+        // 1.编写Java程序，实现判断变量x是奇数还是偶数。
+        int x = 9;
+        if(x%2==0){
+            System.out.println(x+"是偶数");
+        }else{
+            System.out.println(x+"是奇数");
+        }
+        // 2.X%2==0?偶数：奇数
+        System.out.println(x%2==0?x+"是偶数":x+"是奇数");
+
     }
     // 测试结果的数据类型
     private static String getType(Object a) {
