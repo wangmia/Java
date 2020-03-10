a15aba71483672335950494a6683ede9e3d5b063
Your Name <you@example.com>
2020-03-10 12:25:11 +0800

    student

--- src/object/Student.java
+++ src/object/Student.java
@@ -7,39 +7,23 @@ package object;
  * 要求:1.求出平均分最高的人的所有信息 2.找出理科成绩最高的人
  */
 public class Student {
-    private String name;
-    private double chineseResult;
-    private double mathResult;
-    private double englishResult;
-    private double scienceResult;
+    private String name; // 学生姓名
+    private double chineseResult;   // 语文成绩
+    private double mathResult;      // 数学成绩
+    private double englishResult;   // 英语成绩
+    private double scienceResult;   // 理科综合
 
     public void setChineseResult(double chineseResult){
-        if(chineseResult<0||chineseResult>100){
-            System.out.println("成绩格式不正确");
-        }else{
             this.chineseResult = chineseResult;
-        }
     }
-    public void setIfMathResult(double mathResult){
-        if(mathResult<0||mathResult>100){
-            System.out.println("成绩格式不正确");
-        }else{
+    public void setMathResult(double mathResult){
             this.mathResult = mathResult;
-        }
     }
-    public void setIfEnglishResult(double englishResult){
-        if(englishResult<0||englishResult>100){
-            System.out.println("成绩格式不正确");
-        }else{
+    public void setEnglishResult(double englishResult){
             this.englishResult = englishResult;
-        }
     }
-    public void setIfScienceResult(double scienceResult){
-        if(scienceResult<0||scienceResult>100){
-            System.out.println("成绩格式不正确");
-        }else{
+    public void setScienceResult(double scienceResult){
             this.scienceResult = scienceResult;
-        }
     }
     public void setName(String name){
         this.name = name;
@@ -64,14 +48,13 @@ public class Student {
 
     // 输出全部信息
     public String toSting(){
-        return "Name="+name+" ,ChineseResult="+chineseResult+" ,MathResult"+mathResult+
-                " ,EnglishResult"+englishResult+" ,ScienceResult"+scienceResult;
+        return "Name= "+name+" ,ChineseResult= "+chineseResult+" ,MathResult= "+mathResult+
+                " ,EnglishResult= "+englishResult+" ,ScienceResult= "+scienceResult;
     }
 
     // 求平均分
     public double average(){
-        double average = (chineseResult+mathResult+englishResult+scienceResult)/4;
-        return average;
+        return (chineseResult+mathResult+englishResult+scienceResult)/4;
     }
 
 }
